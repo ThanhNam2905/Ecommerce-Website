e1bc8984bdb72763c67ac7ea5e68dcb6580b6c73
nam <you@example.com>
2022-07-21 23:05:01 +0700

    Complete Setup file in project

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: ["./src/**/*.{html,js}"],
+  mode: 'jit',
+  purge: [
+    './public/**/*.html',
+    './src/**/*.{js,jsx,ts,tsx,vue}',
+    './*.html',
+  ],
   theme: {
     extend: {},
   },
