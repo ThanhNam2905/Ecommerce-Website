ff4c3d00a27a01bcf55e2fc295ce9fc286c75ea6
nam <you@example.com>
2022-07-22 16:08:55 +0700

    Add: complete Dropdown menu and section Banner

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,38 +1,44 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
-  mode: 'jit',
-  purge: [
-    './public/**/*.html',
-    './src/**/*.{js,jsx,ts,tsx,vue}',
-    './*.html',
-  ],
-  theme: {
-    screens: {
-      sm: '576px',
-      md: '768px',
-      lg: '991px',
-      xl: '1220px'
+    content: ["./src/**/*.{html,js}"],
+    mode: 'jit',
+    purge: [
+        './public/**/*.html',
+        './src/**/*.{js,jsx,ts,tsx,vue}',
+        './*.html',
+    ],
+    theme: {
+        screens: {
+            sm: '576px',
+            md: '768px',
+            lg: '991px',
+            xl: '1220px'
+        },
+        container: {
+            center: true,
+            padding: {
+                DEFAULT: '1rem',
+                sm: '2rem',
+                lg: '2rem',
+                xl: '2rem',
+                '2xl': '3rem',
+            }
+        },
+        extend: {
+            fontFamily: {
+                roboto: "'Roboto', sans-serif;",
+                'roboto-mono': "'Roboto Mono', monospace;"
+            },
+            colors: {
+                'primary-color': '#FD3D57'
+            }
+        },
     },
-    container: {
-      center: true,
-      padding: {
-        DEFAULT: '1rem',
-        sm: '2rem',
-        lg: '2rem',
-        xl: '2rem',
-        '2xl': '3rem',
-      }
+    variants: {
+        extend: {
+            display: ["group-hover"],
+            visibility: ['group-hover'],
+        },
     },
-    extend: {
-      fontFamily: {
-        roboto: "'Roboto', sans-serif;",
-        'roboto-mono': "'Roboto Mono', monospace;"
-      },
-      colors: {
-        'primary-color': '#FD3D57'
-      }
-    },
-  },
-  plugins: [],
+    plugins: [],
 }
